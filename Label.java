0c060b6ae5d023b85f14b6755aa3cb10f6a3a44b
QirongSu <47405130+QirongSu@users.noreply.github.com>
2019-05-29 17:17:51 -0400

    Add files via upload
    label class

---
+++ Label.java
@@ -0,0 +1,50 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+/**
+ * Write a description of class Label here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class Label extends Actor
+{
+    private String text;
+    private int fontSize;
+    private Color textColor;
+    private Color bkColor;
+    /**
+     * Act - do whatever the Label wants to do. This method is called whenever
+     * the 'Act' or 'Run' button gets pressed in the environment.
+     */
+    public Label(String setText,int fontSizeSet){//label constructor
+        super();
+        text = setText;
+        fontSize = fontSizeSet;
+        textColor = Color.BLACK;
+        bkColor = new Color(0,0,0,0);//transparent background
+        
+        GreenfootImage label = new GreenfootImage(text,fontSize, textColor, bkColor);
+        
+        updateImage();
+    }
+    public void updateImage(){
+        GreenfootImage img = new GreenfootImage(text, fontSize, textColor, bkColor);
+        setImage(img);
+        
+    }
+    public void setText(String textSet){
+        text = textSet;
+        updateImage();
+    }
+    public void setFontSize(int fontSizeSet){
+        fontSize = fontSizeSet;
+        updateImage();
+    }
+    public void setTextColor(Color colorSet){
+        textColor = colorSet;
+        updateImage();
+    }
+    public void act() 
+    {
+        // Add your action code here.
+    }    
+}
