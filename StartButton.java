4d9afb470798b17ebeedfe61a35cf13e0bf778e9
QirongSu <47405130+QirongSu@users.noreply.github.com>
2019-05-29 17:19:29 -0400

    Add files via upload
    startbutton

---
+++ StartButton.java
@@ -0,0 +1,37 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+/**
+ * Write a description of class StartButton here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class StartButton extends Button
+{
+    private boolean sliding = false;
+    private GreenfootImage title = new GreenfootImage("startButton.png");
+    public StartButton(){
+        title.scale(200,150);
+        setImage(title);
+    }
+    public void removeButton(){
+     this.getWorld().removeObject(this);   
+    }
+    public void startSlideOff(){
+        sliding = true;
+        
+    }
+    /**
+     * Act - do whatever the StartButton wants to do. This method is called whenever
+     * the 'Act' or 'Run' button gets pressed in the environment.
+     */
+    public void act() 
+    {
+        if (sliding){
+            this.setLocation(this.getX() + 1, this.getY());
+        }
+        if (Greenfoot.mouseClicked(this)) {
+            this.removeButton();
+        }
+    }    
+}
