aa1901160f2cf9df8cafd6973a6ad7aae937cd8a
gornik-maor <47399494+gornik-maor@users.noreply.github.com>
2019-05-29 18:01:53 -0400

    Added timer to the game to display elapsed time

---
+++ Timer.java
@@ -0,0 +1,36 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+import java.time.*;
+/**
+ * Write a description of class Timer here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class Timer extends Actor
+{
+    private int gameTime;
+    
+    LocalTime timer = LocalTime.MIDNIGHT; // Creating an object of type LocalTime which will store the number of HH/MM/SS Passed since
+    GreenfootImage textImage;
+    
+    public Timer() {
+        textImage = new GreenfootImage("Timer: 00:00:00", 30, Color.BLACK, Color.WHITE);
+        setImage(textImage);
+    }
+
+    /**
+     * Act - do whatever the Timer wants to do. This method is called whenever
+     * the 'Act' or 'Run' button gets pressed in the environment.
+     */
+    public void act() 
+    {
+        gameTime++;
+        
+        if(((SnakeWorld) getWorld()).getGameFrames() % 60 == 0) {
+            timer = timer.plusSeconds(1);
+            textImage = new GreenfootImage("Timer: " + timer, 30, Color.BLACK, Color.WHITE);
+            setImage(textImage);
+        }
+        
+    }    
+}
